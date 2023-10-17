11bb9ab14661780834b034a3a1020ad39765df7c
code-fusheng <2561035977@qq.com>
2023-10-17 11:14:30 +0800

    bak

--- sync_2_min_robot_delete.sh
+++ sync_2_min_robot_delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ubuntu 20.04 noetic
-rsync --delete -avz --progress . code@192.168.1.132:/home/code/htc-robot-ros_ws/src
+rsync --delete -avz --progress . code@192.168.1.102:/home/code/htc-robot-ros_ws/src
