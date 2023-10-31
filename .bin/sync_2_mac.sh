1630a2849fcad55a9672d6798c83d79b269ef035
code-fusheng <2561035977@qq.com>
2023-10-31 18:29:07 +0800

    fix : tf & urdf

--- .bin/sync_2_mac.sh
+++ .bin/sync_2_mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # # waka_6366cf14-6d76-4721-8712-01423fd92a4b
-rsync -avz --progress --exclude={'../CMakeLists.txt','.git'} . fusheng@192.168.1.131:/Users/fusheng/WorkSpace/CompanyWork/work-fusheng/robot-pro/htc-robot-ros_ws/src
+rsync -avz --progress . fusheng@192.168.1.131:/Users/fusheng/WorkSpace/CompanyWork/work-fusheng/robot-pro/htc-robot-ros_ws/src
--- .bin/sync_2_min_robot_delete.sh
+++ .bin/sync_2_min_robot_delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ubuntu 20.04 noetic
-rsync --delete -avz --progress --exclude={'../CMakeLists.txt','.git'} . code@192.168.1.102:/home/code/htc-robot-ros_ws/src
+rsync --delete -avz --progress . code@192.168.1.102:/home/code/htc-robot-ros_ws/src
